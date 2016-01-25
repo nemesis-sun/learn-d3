16b1b308dd19f4904e17f28e65a4ba5970292a53
nemesis-sun <nemesis.sun.now@gmail.com>
2016-01-25 16:18:47 +0800

    gdp trend for multiple countries

--- public/chart.js
+++ public/chart.js
@@ -1,13 +1,13 @@
-var data = [10, 30, 90, 420];
+
 
 var barWidth = 20;
 var countryCodes = [];
 var topCountries = 10;
 
 //this is the group containing the entire chart
-var chart = d3.selectAll(".chart").attr("width", "1000px").attr("height", "1000px").
+var chart = d3.selectAll(".chart").attr("width", "2000px").attr("height", "1000px").
 append("g").
-attr("transform", "translate(100, 50)");
+attr("transform", "translate(150, 50)");
 
 //scaleY for scatter plot cy and y axis
 //the bigger the domain, the smaller the range as the higher point will have smaller cy value
@@ -86,14 +86,19 @@ function processData(data){
 
 function drawChartsForAllCountries(countries){
 
+    //map a 'g' element to each country, with country id as key
     var countryCharts = chart.selectAll(".individual-country-chart").data(countries, function(d){
         return d.id;
     });
 
-    countryCharts.enter().append("g") //create one 'g' element for each country
-    .attr("class", "individual-country-chart")
-    .selectAll("circle").data(function(d){ return d.gdp;}) //map circles to the gdp array of each country
+    //for each new country, create one 'g' element
+    var newCountryCharts = countryCharts.enter().append("g")
+    .attr("class", "individual-country-chart");
+
+    //map a circle to each gdp value in country
+    newCountryCharts.selectAll("circle").data(function(d){ return d.gdp;})
     .enter().append("circle")
+    .attr("class", "trend-line-point")
     .attr("cx", function(d, i) {
         return scaleXOrdinal(d.year);
     })
@@ -103,10 +108,129 @@ function drawChartsForAllCountries(countries){
     .attr("r", 0)
     .transition().attr("r", 3);
 
+    newCountryCharts.selectAll("text").data(function(d){ return d.gdp;})
+    .enter().append("text")
+    .attr("class", "trend-line-text")
+    .attr("x", function(d){
+        return scaleXOrdinal(d.year);
+    })
+    .attr("y", function(d){
+        return scaleY(d.value);
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d){
+        return numeral(d.value).format('($0.00a)');
+    })
+    .style("display", "none");
+
+
+    //updated country, update all data points
+    countryCharts.selectAll('circle').data(function(d){return d.gdp;})
+    .attr("cx", function(d, i){
+        return scaleXOrdinal(d.year);
+    })
+    .attr("cy", function(d, i){
+        return scaleY(d.value);
+    });
+
+    countryCharts.selectAll('text').data(function(d){return d.gdp;})
+    .attr("x", function(d){
+        return scaleXOrdinal(d.year)-5;
+    })
+    .attr("y", function(d){
+        return scaleY(d.value)-5;
+    })
+    .text(function(d){
+        return numeral(d.value).format('($0.00a)');
+    })
+    .style("display", "none");
+
+
+    //remove 'g' of exiting country
+    var oldCountryCharts = countryCharts.exit();
+    oldCountryCharts.remove();
+
+    drawTrendLines(countries);
+}
+
+function drawTrendLines(countries){
+
+    var countryTrendLineData = [];
+
+    var getLineD = d3.svg.line()
+        .x(function(d) {
+            return d.x;
+        })
+        .y(function(d) {
+            return d.y;
+        })
+        .interpolate("linear");
+
+    for(var i=0; i<countries.length; i++){
+        var gdp = countries[i].gdp;
+
+        var lineData = {};
+        lineData.id = countries[i].id;
+        lineData.xy = [];
+
+        for(var j=gdp.length-1; j>=0; j--){
+            lineData.xy.push({
+                x: scaleXOrdinal(gdp[j].year),
+                y: scaleY(gdp[j].value)
+            });
+        }
+        //console.log(JSON.stringify(lineData));
+        countryTrendLineData.push(lineData);
+    }
+
+    //chart.selectAll(".trend-line").remove();
+
+
+
+    var countryTrendLines = chart.selectAll(".individual-country-chart").data(countryTrendLineData, function(d){return d.id;});
+
+    console.log(countryTrendLines);
+
+    countryTrendLines.selectAll(".trend-line").remove();
+
+
+    countryTrendLines
+    .append("path")
+    .attr("class", "trend-line")
+    .attr("d", function(d) {
+        //console.log(JSON.stringify(d.xy));
+        return getLineD(d.xy);
+    })
+    .attr("stroke-dasharray", function(){
+        var pathLength = this.getTotalLength();
+        return pathLength+" "+pathLength;
+    })
+    .attr("stroke-dashoffset", function(){
+        return this.getTotalLength();
+    });
+
+
+    // for(i=0; i<countryTrendLineData.length; i++){
+    //     var path = chart.append("path")
+    //     .attr("class", "trend-line")
+    //     .attr("d", countryTrendLineData[i]);
+
+    //     var pathLength = path.node().getTotalLength();
+
+    //     path.attr("stroke-dasharray", pathLength + " " + pathLength)
+    //         .attr("stroke-dashoffset", pathLength);
+    // }
+
+    chart.selectAll(".trend-line").transition().duration(500)
+        .ease("linear")
+        .attr("stroke-dashoffset", 0);
 
 }
 
 function drawAxises(minGDP, maxGDP){
+
+    chart.selectAll(".axis").remove();
+
     scaleY = scaleY.domain([minGDP, maxGDP]);
 
     //generate x and y axises
@@ -117,7 +241,10 @@ function drawAxises(minGDP, maxGDP){
 
     var yAxis = d3.svg.axis()
         .scale(scaleY)
-        .orient("left");
+        .orient("left")
+        .tickFormat(function(d){
+            return numeral(d).format('($0a)');
+        });
 
     //append x and y axises
     chart.append("g")
@@ -128,7 +255,26 @@ function drawAxises(minGDP, maxGDP){
     chart.append("g")
         .attr("class", "axis")
         .call(yAxis);
+
+
+    chart.selectAll(".y-axis-label").remove();
+    chart.append("text")
+        .attr("class", "y-axis-label")
+        .text("GDP")
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "end")
+        .attr("y", 10);
 }
+
+$(".chart").on("mouseenter", ".trend-line", function(){
+    console.log(".trend-line hover");
+    $(this).parent().find(".trend-line-text").css("display", "");
+});
+
+$(".chart").on("mouseleave", ".trend-line", function(){
+    console.log(".trend-line hover");
+    $(this).parent().find(".trend-line-text").css("display", "none");
+});
 
 // function drawChart(){
 //     var join = chart.selectAll("circle").data(data);
